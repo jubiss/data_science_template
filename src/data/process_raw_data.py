9da5191e02fcb7fd675f7fcf2c5080500e33a636
jose.ferreira <jose.edivaldo.fisica@gmail.com>
2023-08-01 15:21:35 -0300

    adjust processing raw data

--- src/data/process_raw_data.py
+++ src/data/process_raw_data.py
@@ -4,28 +4,27 @@ import pandas as pd
 from src.utils import utils
 
 def process_raw_intern_data(save=False, file_name='intern_table'):
-    table_path = ''
+    table_path = r'data\raw\simulated_listings1.csv'
     internal_processed_table = pd.read_csv(table_path)
     if save:
         utils.save_csv(internal_processed_table, filepath_name=f'data/processed/{file_name}.csv', save=save)
     return internal_processed_table
 
 def process_raw_extern_data(save=False, file_name='intern_table'):
-    table_path = ''
+    table_path = r'data\external\enderecos_problema.csv'
     internal_processed_table = pd.read_csv(table_path)
     if save:
         utils.save_csv(internal_processed_table, filepath_name=f'data/processed/{file_name}.csv', save=save)
     return internal_processed_table
 
-def merge_internal_external_data(save=False, file_name='final_table'):
+def merge_internal_external_data(intern_data, external_data, save=False, file_name='final_table'):
 
-    intern_data = process_raw_intern_data(save=save, file_name='internal_table')
-    external_data = process_raw_extern_data(save=save, file_name='external_table')
-
-    int_ext_data = intern_data.merge(external_data)
+    int_ext_data = intern_data.merge(external_data, on=['latitude', 'longitude'], how='left')
     if save:
         utils.save_csv(int_ext_data, filepath_name=f'data/processed/{file_name}.csv', save=save)
     return int_ext_data
 
 if __name__ == '__main__':
-    merge_internal_external_data(save=True, file_name='processed_table')
+    intern_data = process_raw_intern_data(save=False, file_name='internal_table')
+    external_data = process_raw_extern_data(save=False, file_name='external_table')
+    merge_internal_external_data(intern_data, external_data, save=True, file_name='listings_with_address')
